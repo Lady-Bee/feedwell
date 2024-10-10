09c975aacfd75448aca5bdefbd484e2c41a20296
Lady-Bee <bisimi16@gmail.com>
2024-10-10 12:53:06 +0100

    utilizing useState

--- feedwell/src/App.js
+++ feedwell/src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
 
 
 import './App.css';
 
 function App() {
+  const [seeDays, setSeeDays] = useState('false');
+  const [weekDays, setWeekDays] = useState('false');
+  const listDays = [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday]
+
+
+
   return (
     <div className="App">
      <h1>FeedWell</h1>
