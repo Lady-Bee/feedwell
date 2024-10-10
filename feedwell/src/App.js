453cbc1808ae9668e894d28e7de8cfe7c914044d
Lady-Bee <bisimi16@gmail.com>
2024-10-10 12:59:00 +0100

    set meal function

--- feedwell/src/App.js
+++ feedwell/src/App.js
@@ -7,7 +7,16 @@ import './App.css';
 function App() {
   const [seeDays, setSeeDays] = useState('false');
   const [weekDays, setWeekDays] = useState('false');
-  const listDays = [Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday]
+  const listDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const [setDish, setSetDish] = useState({
+    Monday: { breakfast: '', lunch: '', dinner: '' },
+    Tuesday: { breakfast: '', lunch: '', dinner: '' },
+    Wednesday: { breakfast: '', lunch: '', dinner: '' },
+    Thursday: { breakfast: '', lunch: '', dinner: '' },
+    Friday: { breakfast: '', lunch: '', dinner: '' },
+    Saturday: { breakfast: '', lunch: '', dinner: '' },
+    Sunday: { breakfast: '', lunch: '', dinner: '' }
+  });
 
 
 
