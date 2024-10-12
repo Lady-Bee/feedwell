a75d9efcc86aa9ae7189f386d3baaaa151d63a3d
Lady-Bee <bisimi16@gmail.com>
2024-10-12 17:00:54 +0100

    update index.js

--- feedwell/src/App.js
+++ feedwell/src/App.js
@@ -73,12 +73,25 @@ function App() {
               onChange={(e) => handleDishChange('breakfast', e.target.value)}
               />
             </div>
+            <div className="meal-input">
+              <label>Lunch: </label>
+              <input
+              type="text"
+              value={dishes[weekDay].lunch}
+              onChange={(e) => handleDishChange('lunch', e.target.value)}
+              />
+            </div>
+            <div className="meal-input">
+              <label>Dinner: </label>
+              <input
+              type="text"
+              value={dishes[weekDay].dinner}
+              onChange={(e) => handleDishChange('dinner', e.target.value)}
+              />
+            </div>
+            <button className="submit-button" onClick={handleSubmit}>Submit Dish</button>
           </div>
-        )
-
-        }
-      
-     
+        )}
     </div>
   );
 }
